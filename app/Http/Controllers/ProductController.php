b9bf8a296bbb746dacba920d51749b1f85bdd907
admin <admin@adminui-MacBookPro-2.local>
2024-01-09 16:30:55 +0900

    게시판 연습

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     //
+    private $product;
+
+    //Laravel 의 IoC
+    public function __construct(Product $product){
+        $this->product = $product;
+    }
+
+    public function index(){
+        //Products의 데이터를 최신순으로 페이징해서 가져온다.
+        $products = $this->product->latest()->paginate(10);
+        // produce/index.blade 에 $products 를 보내준다.
+        return view('products.index', compact('products'));
+    }
+
+    public function create(){
+        return view('products.create');
+    }
+
+    public function store(Request $request){
+        $request = $request->validate([
+            'name' => 'required',
+            'content' => 'required'
+        ]);
+
+        $this->product->create($request);
+        return redirect()->route('products.index');
+    }
 }
--- database/migrations/2024_01_09_030747_create_products_table.php
+++ database/migrations/2024_01_09_030747_create_products_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 64);
+            $table->string('content', 256);
             $table->timestamps();
         });
     }
